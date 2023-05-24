5b14afeae0eb3dc76ba8debabf46dc00a617f5fb
arigatosimarmata <arigatosimarmata5@gmail.com>
2023-05-24 13:42:54 +0700

    optimize logic cacheinfile

--- cacheinfile.go
+++ cacheinfile.go
@@ -16,7 +16,7 @@ func Set(cache_directory string, key string, data string, expire time.Duration) 
 	file := fmt.Sprintf("fcache.%s", key)
 	// file := key
 
-	fpath := filepath.Join(cache_directory, file)
+	// fpath := filepath.Join(cache_directory, file)
 
 	if _, err := os.Stat(cache_directory); os.IsNotExist(err) {
 		if err := os.MkdirAll(cache_directory, 0755); err != nil {
@@ -24,15 +24,24 @@ func Set(cache_directory string, key string, data string, expire time.Duration) 
 		}
 	}
 
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		log.Fatal(err)
+	}
+
+	fp, err := os.Create(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	var fmutex sync.RWMutex
 	fmutex.Lock()
 	defer fmutex.Unlock()
-	fp, err := os.OpenFile(fpath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
-	if err != nil {
-		return err
-	}
+	// fp, err := os.OpenFile(fpath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	// if err != nil {
+	// 	return err
+	// }
 
-	defer fp.Close()
+	// defer fp.Close()
 	if _, err = fp.WriteString(data); err != nil {
 		fp.Close()
 		return err
@@ -59,19 +68,18 @@ func Get(cache_directory string, key string, dst string) (bool, string, error) {
 		return false, "", err
 	}
 
-	fp, err := os.OpenFile(files[0], os.O_RDONLY, 0400)
-	if err != nil {
-		return false, "", err
-	}
-	defer fp.Close()
+	// fp, err := os.OpenFile(files[0], os.O_RDONLY, 0400)
+	// if err != nil {
+	// 	return false, "", err
+	// }
+	// defer fp.Close()
 
 	datafile, err := os.ReadFile(files[0]) // just pass the file name
 	if err != nil {
-		fp.Close()
 		return false, "", err
 	}
 
 	data_out := string(datafile)
-	fp.Close()
+	// fp.Close()
 	return true, data_out, nil
 }
